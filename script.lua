07ebdeb1731055d6fcace618d97cfb8cdf7e0f01
mv.qna <91986184+mvqna@users.noreply.github.com>
2025-01-19 15:28:48 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,25 +1,17 @@
-local StarterGui = game:GetService("StarterGui")
-
--- Function to send a notification
-local function sendNotification(title, text, duration)
-    StarterGui:SetCore("SendNotification", {
-        Title = title; 
-        Text = text; 
-        Duration = duration; 
-    })
-end
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro2/main/ui2"))() 
+local NotifyLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vKhonshu/intro/main/ui"))()
 
 -- Get the executor name
-local executorname = getexecutorname() or identifyexecutor()
+local executorname = getexecutorname() or identifyexecutor() or "Unknown Executor"
 
 -- Supported executors
 local supportedExecutors = { AWP = true, Wave = true, Nihon = true, ["Synapse Z"] = true, Swift = true }
 
 -- Check if executor is supported
 if supportedExecutors[executorname] then
-    sendNotification(executorname .. " is supported!", "", 5)
+    NotifyLib.prompt('Executor Supported', executorname .. " is supported!", 5)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/mvqna/another-hub/refs/heads/main/main/loader.lua'))()
 else
-    sendNotification("Executor Might Not Be Supported", "Expect Errors", 5)
+    NotifyLib.prompt('Executor Not Supported', executorname .. " is not supported, expect errors.", 5)
     loadstring(game:HttpGet('https://raw.githubusercontent.com/mvqna/another-hub/refs/heads/main/main/loader.lua'))()
 end
