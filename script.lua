fb58690042e8069ddaf373d695a6697ab71938b6
mv.qna <91986184+mvqna@users.noreply.github.com>
2025-01-19 15:07:21 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,25 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mvqna/another-hub/refs/heads/main/main/loader.lua", true))()
+local StarterGui = game:GetService("StarterGui")
+
+-- Function to send a notification
+local function sendNotification(title, text, duration)
+    StarterGui:SetCore("SendNotification", {
+        Title = title; 
+        Text = text; 
+        Duration = duration; 
+    })
+end
+
+-- Get the executor name
+local executorname = getexecutorname() or identifyexecutor()
+
+-- Supported executors
+local supportedExecutors = { AWP = true, Wave = true, Nihon = true, ["Synapse Z"] = true, Swift = true }
+
+-- Check if executor is supported
+if supportedExecutors[executorname] then
+    sendNotification(executorname .. " is supported!", "", 5)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/mvqna/another-hub/refs/heads/main/main/loader.lua'))()
+else
+    sendNotification("Executor Might Not Be Supported", "Expect Errors", 5)
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/mvqna/another-hub/refs/heads/main/main/loader.lua'))()
+end
